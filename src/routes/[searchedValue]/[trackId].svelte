14d019b2d240c3186502f138e54758efd233e21f
sabbirz <sabbir.kuasha@gmail.com>
2022-08-06 01:25:17 +0600

    Player Added
    basic player added

--- src/routes/[searchedValue]/[trackId].svelte
+++ src/routes/[searchedValue]/[trackId].svelte
@@ -1,6 +1,7 @@
 <script>
     import {page} from '$app/stores'
     import {onMount} from 'svelte'
+    import AudioPlayer from '$lib/AudioPlayer.svelte'
 
 
     let songResults = []
@@ -20,10 +21,35 @@
 
 
 
-<section>
-    This is the song id: {$page.params.trackId}
-    <hr>
+<section class="text-center">
+    <div class="p-5">
+        The song id: {$page.params.trackId}
+    </div>
+    
+
     {#each songResults as songResult}
-        {songResult.artistName}
+        <div class=" flex flex-col justify-center">
+            <img class="w-60 m-auto" src={songResult.artworkUrl100} alt={songResult.artistName}>
+            <h1>{songResult.artistName}</h1>
+            <div class="m-auto">
+                <audio controls>
+                    <source src={songResult.previewUrl} type="audio/ogg">
+                </audio>
+            </div>
+            
+        </div>
+        
+        <div class="flex flex-col w-full border-opacity-50">
+            <div class="divider">OR</div>
+        </div>
+
+        <AudioPlayer    trackName = {songResult.trackCensoredName}
+                        albumName = {songResult.collectionName}
+                        artistName = {songResult.artistName} 
+                        artworkurl = {songResult.artworkUrl100}
+                        trackpreviewUrl = {songResult.previewUrl}              
+        />
     {/each}
+
+    
 </section>
